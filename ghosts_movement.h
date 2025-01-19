6efae3b74f17d7263f839578e0becf32f9ed410b
amirpasha1969 <amirpashagt600@gmail.com>
2025-01-19 22:02:29 +0330

    Delete ghosts_movement.h

--- ghosts_movement.h
+++
@@ -1,9 +0,0 @@
-#ifndef GHOSTS_MOVEMENT_H
-#define GHOSTS_MOVEMENT_H
-#include "map.h"
-
-void chaseplayer(int playerX, int playerY, int *ghostX, int *ghostY, float *timer, float delay);
-void random_move(int *ghostX, int *ghostY, int ghostIndex, int ghostCount, int ghostPositions[][2], char map[SIZE1][SIZE2], float *timer, float delay, int *direction); 
-
-
-#endif
